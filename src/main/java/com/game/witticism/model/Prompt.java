03acb86bb7d427891e488cff5d67a483fa221b17
Mariah Legg <mariah.legg@gmail.com>
2022-01-27 15:15:12 -0500

    Add prompt model.

---
+++ src/main/java/com/game/witticism/model/Prompt.java
@@ -0,0 +1,34 @@
+package com.game.witticism.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "prompt")
+public class Prompt {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String text;
+
+    public Prompt() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
