454dcf7613d1a93090c5b366a73357b20ca1b875
Mariah Legg <mariah.legg@gmail.com>
2022-01-27 15:12:29 -0500

    Add player model.

---
+++ src/main/java/com/game/witticism/model/Player.java
@@ -0,0 +1,45 @@
+package com.game.witticism.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "player")
+public class Player {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private boolean isHost;
+
+    public Player() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isHost() {
+        return isHost;
+    }
+
+    public void setHost(boolean host) {
+        isHost = host;
+    }
+}
