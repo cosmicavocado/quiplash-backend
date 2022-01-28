2236b78da5a1b73236b96ae4d43a52076ed42f71
Mariah Legg <mariah.legg@gmail.com>
2022-01-28 16:06:26 -0500

    Add player repository.

---
+++ src/main/java/com/game/witticism/repository/PlayerRepository.java
@@ -0,0 +1,7 @@
+package com.game.witticism.repository;
+
+import com.game.witticism.model.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+}
