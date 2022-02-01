9b72dee718413826a2d48caf529f5a3b6037bffe
Mariah Legg <mariah.legg@gmail.com>
2022-02-01 17:05:47 -0500

    Add find by code

--- src/main/java/com/game/witticism/repository/GameRepository.java
+++ src/main/java/com/game/witticism/repository/GameRepository.java
@@ -4,4 +4,5 @@ import com.game.witticism.model.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
+    Game findByCode(String code);
 }
