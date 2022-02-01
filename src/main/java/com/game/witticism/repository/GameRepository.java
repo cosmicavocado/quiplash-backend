7adc4977cb7d05b276feb3d44b84b1cd00e29237
Mariah Legg <mariah.legg@gmail.com>
2022-02-01 15:35:08 -0500

    Correct Jpa declaration: Player to Game

--- src/main/java/com/game/witticism/repository/GameRepository.java
+++ src/main/java/com/game/witticism/repository/GameRepository.java
@@ -1,7 +1,7 @@
 package com.game.witticism.repository;
 
-import com.game.witticism.model.Player;
+import com.game.witticism.model.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GameRepository extends JpaRepository<Player, Long> {
+public interface GameRepository extends JpaRepository<Game, Long> {
 }
