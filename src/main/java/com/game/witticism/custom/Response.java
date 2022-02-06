8593cc8e5f55f31ed6fa7231fcf95aabf6d86378
Mariah Legg <mariah.legg@gmail.com>
2022-02-06 18:44:50 -0500

    Update response model

--- src/main/java/com/game/witticism/custom/Response.java
+++ src/main/java/com/game/witticism/custom/Response.java
@@ -3,43 +3,27 @@ package com.game.witticism.custom;
 import java.util.List;
 
 public class Response {
-    private int round;
-    private Long promptId;
-    private Long playerId;
+    private String promptText;
+    private String playerName;
     private String responseText;
-    private List<Long> votes;
 
     public Response() {
     }
 
-    public Response(Long promptId, Long playerId, String responseText) {
-        this.promptId = promptId;
-        this.playerId = playerId;
-        this.responseText = responseText;
+    public String getPromptText() {
+        return promptText;
     }
 
-    public int getRound() {
-        return round;
+    public void setPromptText(String promptText) {
+        this.promptText = promptText;
     }
 
-    public void setRound(int round) {
-        this.round = round;
+    public String getPlayerName() {
+        return playerName;
     }
 
-    public Long getPromptId() {
-        return promptId;
-    }
-
-    public void setPromptId(Long promptId) {
-        this.promptId = promptId;
-    }
-
-    public Long getPlayerId() {
-        return playerId;
-    }
-
-    public void setPlayerId(Long playerId) {
-        this.playerId = playerId;
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
     }
 
     public String getResponseText() {
@@ -48,13 +32,5 @@ public class Response {
 
     public void setResponseText(String responseText) {
         this.responseText = responseText;
-    }
-
-    public List<Long> getVotes() {
-        return votes;
-    }
-
-    public void setVotes(List<Long> votes) {
-        this.votes = votes;
     }
 }
